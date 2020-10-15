63fd979f20a857740e324e1bc603bc52c39251c5
FahimHassanMollah <fahimhassan0197@gmail.com>
2020-10-15 09:13:46 +0600

    service status update api added

--- index.js
+++ index.js
@@ -2,20 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+let ObjectId = require('mongodb').ObjectID;
 const app = express();
-
 const port = 8080
-
 app.use(cors());
 app.use(bodyParser());
 app.use(bodyParser.json());
 require('dotenv').config();
-
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jrzhf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
@@ -26,17 +22,13 @@ client.connect(err => {
     app.post('/saveOrderInformations', (req, res) => {
         orderCollection.insertOne(req.body)
             .then(result => {
-
                 res.send(result.insertedCount > 0);
-
             })
     });
     app.post('/addService', (req, res) => {
         serviceCollection.insertOne(req.body)
             .then(result => {
-
                 res.send(result.insertedCount > 0);
-
             })
     });
     app.get('/getUserServiceListByEmail/:email', (req, res) => {
@@ -44,7 +36,6 @@ client.connect(err => {
         orderCollection.find({ email: req.params.email })
             .toArray((err, documents) => {
                 res.send(documents);
-
             })
     });
     app.get('/getServiceImage', (req, res) => {
@@ -52,53 +43,52 @@ client.connect(err => {
         serviceCollection.find({})
             .toArray((err, documents) => {
                 res.send(documents);
-
             })
     });
     app.post('/addReview', (req, res) => {
         reviewCollection.insertOne(req.body)
             .then(result => {
-
                 res.send(result.insertedCount > 0);
-
             })
     });
     app.get('/getAllReview', (req, res) => {
         reviewCollection.find({})
             .toArray((err, documents) => {
                 res.send(documents);
-
             })
     });
     app.get('/getAllOrderInformation', (req, res) => {
         orderCollection.find({})
             .toArray((err, documents) => {
-
                 res.send(documents);
-
             })
     });
     app.post('/addAdmin', (req, res) => {
         adminCollection.insertOne(req.body)
             .then(result => {
-
                 res.send(result.insertedCount > 0);
-
             })
     });
     app.post('/getAdmin', (req, res) => {
-        // console.log(req.body,'body');
         adminCollection.find({email:req.body.email})
         .toArray((err, documents) => {
            res.send(documents.length>0)
-
         })
     });
-
+    app.patch('/updateOrderStatus', (req, res) => {
+        orderCollection.updateOne({ _id: ObjectId(req.body.id) },
+          {
+            $set: { status: req.body.status}
+          })
+          .then(result => {
+            if (result.modifiedCount>0) {
+               res.send(true);
+            }
+          })
+      }
+      )
     console.log('working ');
 });
-
-
 app.listen(process.env.PORT || port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 }) 
